6a987066bf8842ca40180dfc567a95f6b5f09753
CogDog <cogdogblog@gmail.com>
2018-08-26 15:16:11 -0600

    Shotcodes
    Added shortcode for artifact type lsting, with docs to Readme

--- README.md
+++ README.md
@@ -2,4 +2,45 @@
 
 * a [cog.dog](https://cog.dog) production
 
-Creates custom post types and taxonomies for Trinity Western University portfolios
+Creates custom post types and taxonomies for Trinity Western University portfolios.
+
+## Shortcodes
+
+The plugin enables use of these shortcodes which can be used in posts, pages, and widgets
+
+
+### Artifact Count
+
+    [artifact_count]
+    
+Displays a current total count of all artifacts in the portfolio, it will produce output if a portfolio contains say 12 artifacts, like:
+
+    12 artifacts
+    
+Adding a parameter `link=1` it will output the same but as a hyperlink to view all the artifacts.
+
+    [artifact_count link=1]
+    
+
+### Artifact types
+
+This shortcode will output a full index of the portfolio structure according to the taxonomy for Artifacts (which can be edited in the WordPress dashboard under **TWU Portfolio** -> **Artifact Types**.
+
+    [artifact_types]
+    
+Will list the full taxonomy, each linked to its archive, with a display of how many artifacts are within that type. The following parameters can be used in the shortcode
+
+* `show_description=1` Show the description of the artifact type (if available). Default is `1` to show, set to `0` to hide.
+* `show_chilcren=1` Display one level of sub-types. Default is `1` to show, set to `0` to hide.
+* `hide_empty=0` Only list types that are used in artifacts. Default is `0` to show all available, set to `1` to show only ones that are used.
+* `order_by='id'` How the types are sorter; `id` (default) is by order they ere created; other options include `name` to sort by title, `count` by count of items.
+* `order` direction of sorting; `ASC` (default) is alphabetical or increasing; used `DESC` for reverse
+
+Example- List full taxonomy without descriptions
+
+    [artifact_types show_description=0]
+    
+
+Example- List only top level taxonomy without descriptions (e.g for a widget).
+
+    [artifact_types show_description=0 show_children=0]
--- twu-portfolio.php
+++ twu-portfolio.php
@@ -82,15 +82,120 @@ function run_twu_portfolio() {
 run_twu_portfolio();
 
 
+// extra custom code. Can't figure yet how to roll it insisde
 add_shortcode( 'artifact_count', 'twu_portfolio_count_artifcats');
 
 function twu_portfolio_count_artifcats( $atts ) {
 
-		extract( shortcode_atts( array( "link" => 0), $atts ) );  
-	
-		if ( $link ) {
-			return '<a href="' . get_post_type_archive_link( 'twu-portfolio' ) . '">' . wp_count_posts('twu-portfolio')->publish  . ' artifacts</a>';
-		} else {
-			return wp_count_posts('twu-portfolio')->publish  . ' artifacts';
-		}
+	extract( shortcode_atts( array( "link" => 0), $atts ) );  
+
+	if ( $link ) {
+		return '<a href="' . get_post_type_archive_link( 'twu-portfolio' ) . '">' . wp_count_posts('twu-portfolio')->publish  . ' artifacts</a>';
+	} else {
+		return wp_count_posts('twu-portfolio')->publish  . ' artifacts';
 	}
+}
+
+add_shortcode( 'artifact_types', 'twu_portfolio_taxonomy_list' );
+
+function twu_portfolio_taxonomy_list( $atts ) {
+
+   extract( shortcode_atts( 
+   		array(
+			'name' => 'twu-portfolio-type',
+			'hide_empty' => false,
+			'orderby' => 'id',
+			'order' => 'ASC',
+			'show_description' => true,
+			'show_children' => true
+   		 ),
+   	$atts ) ); 
+   		 
+   		 
+    
+    $args = array(
+        'taxonomy' => $name,
+        'hide_empty' => $hide_empty,
+        'orderby' =>    $orderby,
+        'order' => $order,
+        'count' => true,
+        'pad_counts' => true,
+        'parent'   => 0
+    );
+
+	$term_query = new WP_Term_Query( $args );
+	
+    $html = '<ul class="type-list">';
+
+    if ( !empty( $term_query->terms ) ) {
+        foreach ( $term_query->terms as $term ) {
+                                        
+            $has_children = false;
+            $child_html = '';
+             
+            if ( twu_portfolio_has_child_terms( $name, $term->term_id, $hide_empty ) == true AND  $show_children == true ) {
+            
+            	$has_children = true;
+            	$child_count = 0;
+                $child_html = '<ul class="type-sublist">';
+            
+            	    $child_args = array(
+						'taxonomy' => $name,
+						'hide_empty' => $hide_empty,
+						'orderby' =>    $orderby,
+						'order' => $order,
+						'count' => true, 
+						'parent'   => $term->term_id
+					);
+
+					$child_term_query = new WP_Term_Query( $child_args );
+
+            			foreach ( $child_term_query->terms as $child_term ) {
+								
+							$child_term_link = get_term_link( $child_term );
+							$child_html .= '<li><a href="' . esc_url( $child_term_link ) . '">' . $child_term->name . '</a> (' . $child_term->count . ')';
+							if ( $child_term->description AND $show_description ) {
+								$child_html .= '<br /><span class="term-description">' .  $child_term->description . '</span>';
+							}
+							
+							$child_count += $child_term->count;
+					
+							$child_html .= '</li>';
+					}
+            
+            	$child_html .= '</ul>';
+            } // has child terms
+  
+  
+              $term_link = get_term_link( $term );
+              
+              $the_count = ( $has_children ) ? $term->count + $child_count :  $term->count;
+  
+              $html .= '<li><a href="' . esc_url( $term_link ) . '">' . $term->name . '</a> (' .  $the_count . ')';
+              
+              // add description if parameter set, and there is one to show
+              if ( $term->description AND $show_description ) {
+             	 $html .= '<br /><span class="term-description">' . $term->description . '</span>';
+			  }
+          
+            $html .=  $child_html . '</li>';
+        } // foreach terms
+        
+   		$html .= '</ul>';      
+    }
+    
+    return $html;
+}
+
+function twu_portfolio_has_child_terms( $taxonomy, $parent_id, $hide_empty ) {
+    $has_child_terms = get_terms( array(
+        'taxonomy' => $taxonomy,
+        'hide_empty' => $hide_empty,
+        'parent'   => $parent_id
+    ) );
+    
+    if( !empty( $has_child_terms ) )
+        return true;
+    else 
+        return false;
+}
